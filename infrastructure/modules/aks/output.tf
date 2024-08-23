8f0bb1fc53bf79f11378397710bcf01c4e82bf14
Ayomide_Abubakar <145975516+Luminous247@users.noreply.github.com>
2024-08-23 01:38:54 +0100

    Delete infrastructure directory

--- infrastructure/modules/aks/output.tf
+++
@@ -1,9 +0,0 @@
-output "kube_config" {
-  description = "The Kubernetes configuration"
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
-
-output "cluster_name" {
-  description = "The name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
-}
